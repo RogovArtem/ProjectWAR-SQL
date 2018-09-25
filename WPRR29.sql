062cd4e1d624cb019d17e441aabdf428095c5c33
Sevetar <Sevatar.Condemned@gmail.com>
2018-09-25 11:13:22 +1000

    Potion of Fame + RR 29 WP weapons

---
+++ WPRR29.sql
@@ -0,0 +1,6 @@
+
+#Inserts lvl RR 29 weapons into the vendor in altdorf
+Insert into war_world.vendor_items  values (252,5500565,0,0,0,'(30,208433)');
+Insert into war_world.vendor_items  values (252,5500535,0,0,0,'(30,208433)');
+Insert into war_world.vendor_items  values (252,5500551,0,0,0,'(30,208433)');
+Insert into war_world.vendor_items  values (252,5500465,0,0,0,'(30,208433)');
