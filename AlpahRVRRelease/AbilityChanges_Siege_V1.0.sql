d4a3e987775057a2d7572ca62cc4695a7f77172e
Ikthaleon <Sevatar.Condemned@gmail.com>
2019-03-17 20:13:39 +1100

    Changes for 63.x release 17MAR19

---
+++ AlpahRVRRelease/AbilityChanges_Siege_V1.0.sql
@@ -0,0 +1,2 @@
+UPDATE `war_world`.`ability_commands` SET `PrimaryValue` = '86210' WHERE (`Entry` = '24770') and (`CommandID` = '0') and (`CommandSequence` = '0');
+UPDATE `war_world`.`ability_commands` SET `PrimaryValue` = '86222' WHERE (`Entry` = '24776') and (`CommandID` = '0') and (`CommandSequence` = '0');
--- Fort_Zone_areas.sql
+++ Fort_Zone_areas.sql
@@ -11,3 +11,11 @@ INSERT INTO `war_world`.`zone_areas` (`AreaName`, `ZoneId`, `AreaId`, `Realm`, `
 INSERT INTO `war_world`.`zone_areas` (`AreaName`, `ZoneId`, `AreaId`, `Realm`, `PieceId`, `OrderInfluenceId`, `DestroInfluenceId`, `TokExploreEntry`) VALUES ('Reikwald', '110', '1', '0', '30', '122', '113', '5840');
 INSERT INTO `war_world`.`zone_areas` (`AreaName`, `ZoneId`, `AreaId`, `Realm`, `PieceId`, `OrderInfluenceId`, `DestroInfluenceId`, `TokExploreEntry`) VALUES ('Fell Landing', '204', '61', '0', '30', '186', '177', '5841');
 INSERT INTO `war_world`.`zone_areas` (`AreaName`, `ZoneId`, `AreaId`, `Realm`, `PieceId`, `OrderInfluenceId`, `DestroInfluenceId`, `TokExploreEntry`) VALUES ('Shining Way', '210', '61', '0', '30', '186', '177', '5842');
+
+
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '100');
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '101');
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '102');
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '103');
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '104');
+UPDATE `war_world`.`keep_infos` SET `IsFortress` = '1' WHERE (`KeepId` = '105');
