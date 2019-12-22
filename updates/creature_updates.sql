edf405b7a3da98d79ec8a981f7f36133a1ff8322
cooler-SAI <cooler19860212@yahoo.com>
2019-12-22 19:27:30 +0300

    more creature's updates

--- updates/creature_updates.sql
+++ updates/creature_updates.sql
@@ -5,3 +5,8 @@ UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '4122';
 UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '661'; 
 UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '3550'; 
 UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '3520'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '3519'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '4216'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '1419'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '287'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '137'; 
