19134ea2e3734c33ad5e8777748c6f4cc8339ee5
cooler-SAI <cooler19860212@yahoo.com>
2019-12-22 19:17:22 +0300

    few creature's move fix

---
+++ updates/creature_updates.sql
@@ -0,0 +1,7 @@
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '384'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '2392';
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '4117'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '4122'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '661'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '3550'; 
+UPDATE `war_world`.`creature_protos` SET `Unk1` = '1' WHERE `Entry` = '3520'; 
